7c06be01d0a855ed1eec67ce870f9572f98a4189
vaibhavdevkar <93572156+vaibhavdevkar@users.noreply.github.com>
2023-10-07 12:25:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Findme from './component/Findme';
 import Education from './component/Education';
 import Skills from './component/Skills';
 import Contact from './component/Contact';
-// import Project from './component/Project';
+
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
       <br /> 
       <Skills />
       <Education />
-      // <Project />
+   
       <Contact />
     </div>
   );
